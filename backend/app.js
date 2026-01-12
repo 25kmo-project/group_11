7af35c8ca9e914e6fa12e6bca358a80ae18e6517
Tonttu81 <joni.pylkkonen@gmail.com>
2026-01-12 14:02:52 +0200

    Alustettu accounts route
    Asennettu myös tarvittavia dependencyjä kuten mysql2, bcryptjs ja dotenv

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var accountsRouter = require('./routes/accounts')
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/accounts', accountsRouter)
 
 module.exports = app;
