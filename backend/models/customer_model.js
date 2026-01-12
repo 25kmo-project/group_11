58d7ee674970c90165c7c33b260f07f6f8cd6875
Tommi <tommi.karsi@gmail.com>
2026-01-12 23:17:01 +0200

    customer CRUD komennot tehty ja testattu, env.example luotu

--- backend/models/customer_model.js
+++ backend/models/customer_model.js
@@ -2,21 +2,21 @@ const db = require('../database');
 
 const customer={
 getAll:function(callback){
-    return db.query("SELECT * FROM customer");
+    return db.query("SELECT * FROM customer", callback);
 },
 getOne:function(idcustomer, callback){
     return db.query("SELECT * FROM customer where idcustomer=?", [idcustomer], callback);
 },
 add:function(customer, callback){
-    return db.query("INSERT INTO customer VALUES (?,?,?,?)", [customer.fname, customer.lname, customer.street_address, customer.city, callback]);
+    return db.query("INSERT INTO customer (fname, lname, street_address, city) VALUES (?,?,?,?)", [customer.fname, customer.lname, customer.street_address, customer.city], callback);
 },
 update:function(customer, idcustomer, callback){
-return db.query("UPDATE FROM customer SET fname=?, lname=?, street_address=?, city=? where idcustomer=?", [customer.fname, customer.lname, customer.street_address, customer.city, idcustomer, callback]);
+return db.query("UPDATE customer SET fname=?, lname=?, street_address=?, city=? where idcustomer=?", [customer.fname, customer.lname, customer.street_address, customer.city, idcustomer], callback);
 },
 
 delete:function(idcustomer, callback){
-return db.query("DELETE customer WHERE idcustomer=?", [idcustomer], callback);
-}
+return db.query("DELETE FROM customer WHERE idcustomer=?", [idcustomer], callback);
+},
 
 };
 
--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const router = express.Router();
 const customer = require('../models/customer_model');
 
-get:('/', function(request, response){
+router.get('/', function(request, response){
     customer.getAll(function(err, result){
         if(err){
             response.send(err);
@@ -10,6 +10,53 @@ get:('/', function(request, response){
         else{
             console.log("tiedonhaku onnistui");
             response.json(result);
+        }
+    })
+});
+
+router.get('/:idcustomer', function(request, response){
+    customer.getOne(request.params.idcustomer, function(err, result){
+        if(err){
+            response.send(err);
+        }
+        else{
+            console.log("tiedonhaku onnistui");            
+            response.json(result[0]);
+        }
+
+    })
+});
+router.post('/', function(request, response){
+    customer.add(request.body, function(err, result){
+        if(err){
+            response.send(err);
+        }
+        else{
+            response.json(result[0]);
+            console.log("lisäys onnistui");
+        }
+    })
+});
+router.put('/:idcustomer', function(request, response){
+    customer.update(request.body, request.params.idcustomer, function(err, result){
+        if(err){
+            response.send(err);
+        }
+        else{
+            response.json(result[0])
+            console.log("päivitys onnistui");
+        }
+    })
+});
+
+router.delete('/:idcustomer', function(request, response){
+    customer.delete(request.params.idcustomer, function(err, result){
+        if(err){
+            response.send(err);
+        }
+        else{
+            response.json(result[0]);
+            console.log("Käyttäjä", request.params.idcustomer, "poistettu");
         }
     })
 });
