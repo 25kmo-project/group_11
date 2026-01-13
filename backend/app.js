84c9990444bdf2372dc12478e20941e3f2626b40
Tonttu81 <joni.pylkkonen@gmail.com>
2026-01-13 15:56:36 +0200

    Poistettu "users"-route

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var accountsRouter = require('./routes/accounts')
 
 var app = express();
@@ -16,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/accounts', accountsRouter)
 
 module.exports = app;
