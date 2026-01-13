f5d6767ff486a74bb1c2d8e349af1d7999f6b95e
Aapo <95102574+Kartsoful@users.noreply.github.com>
2026-01-13 20:38:31 +0200

    Routeen lisätty /api

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/card', cardRouter);
+app.use('/api/card', cardRouter);
 app.use('/api/accounts', accountsRouter)
 
 module.exports = app;
