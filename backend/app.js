8f490b5ab5c1604a3e49332d833e74b543e38679
Tkarsikas <tommi.karsi@gmail.com>
2026-01-16 17:18:54 +0200

    account_customer enpointtia edistetty. ei valmis

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var customerRouter = require ('./routes/customer');
 var cardRouter = require('./routes/card')
 var accountsRouter = require('./routes/accounts')
+var account_customerRouter = require('./routes/account_customer');
 
 
 var app = express();
@@ -21,5 +22,6 @@ app.use('/', indexRouter);
 app.use('/api/customer', customerRouter);
 app.use('/api/card', cardRouter);
 app.use('/api/accounts', accountsRouter)
+app.use('/api/account_customer', account_customerRouter);
 
 module.exports = app;
---
+++ backend/routes/account_customer.js
@@ -0,0 +1,65 @@
+const express = require ('express');
+const router = express.Router();
+const account_customer = require ('../models/account_customer_model');
+const validateFields = require ('../middleware/validateFields');
+
+const required_fields = ['customer_id', 'account_id'];
+
+router.get('/', function(request, response){
+    account_customer.getAll(function(err, result){
+        if(err){
+            return response.status(500).json({ status_code: response.statusCode, message: err });
+        }
+        else{
+            response.json(result);
+        }
+    })
+});
+
+router.get('/:idaccount_customer', function(request, response){
+    account_customer.getOne(request.params.idaccount_customer, function(err, result){
+        if(err){
+            return response.status(500).json({ status_code: response.statusCode, message: err });
+        }
+        else{
+            response.json(result);
+        }
+    })
+});
+
+router.post('/', validateFields(required_fields), function(request, response){
+    account_customer.add(request.body, function(err, result){
+        if(err){
+            response.status(500).json({ status_code: response.statusCode, message: err });
+        }
+        else{
+            response.json(result);
+        }
+    })
+});
+
+router.put('/:idaccount_customer', validateFields(required_fields), function(request,response){
+    account_customer.update(request.body, request.params.idaccount_customer, function(err, result){
+        if(err){
+            response.status(500).json({status_code: response.statusCode, message: err });
+        }
+        else{
+            response.json(result)
+        }
+    })
+});
+
+router.delete('/:idaccount_customer', function(request, response){
+    account_customer.delete(request.params.idaccount_customer, function(err, result){
+        if(err){
+            response.status(500).json({status_code: response.statusCode, message: err });
+        }
+        else{
+            response.json(result)
+        }
+    })
+});
+
+router.patch()
+
+module.exports=router;
