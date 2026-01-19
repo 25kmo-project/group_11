167e024215d26c0107d67cf6f7a891d612b89543
Jesul1 <omajesse.vaarala@gmail.com>
2026-01-19 18:07:13 +0200

    whoops

--- backend/swagger/swagger.js
+++ backend/swagger/swagger.js
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "My Express API",
+      title: "Bank database API",
       version: "1.0.0",
-      description: "API documentation for my Express app",
+      description: "API documentation for a bank database",
     },
 
     tags: [
@@ -45,7 +45,6 @@ const options = {
     },
   },
   apis: ["./swagger/api_docs/*.yaml", "./swagger/swagger.js"], // files with annotations
-
 };
 
 const swaggerSpec = swaggerJsdoc(options);
