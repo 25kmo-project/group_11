ca25a2a154f57332be4be04cc0860981354d16c9
Tommi <tommi.karsi@gmail.com>
2026-01-22 00:16:39 +0200

    swagger tehty, CRUD valmis ja testattu

--- backend/routes/account_customer.js
+++ backend/routes/account_customer.js
@@ -18,23 +18,30 @@ router.get('/', function(request, response){
 
 router.get('/:idaccount_customer', function(request, response){
     account_customer.getOne(request.params.idaccount_customer, function(err, result){
-        if(err){
+        if (err) {
             return response.status(500).json({ status_code: response.statusCode, message: err });
         }
-        else{
-            response.json(result);
+
+        if (result.length === 0) {
+            return response.status(404).json({ status_code: response.statusCode, message: 'User not found.' });
         }
+
+        response.json(result);
     })
 });
 
 router.post('/', validateFields(required_fields), function(request, response){
     account_customer.add(request.body, function(err, result){
-        if(err){
-            response.status(500).json({ status_code: response.statusCode, message: err });
+        if (err) {
+            error_message = err;
+        if (err.errno === 1062) {
+            error_message = 'Cannot create an account with the same ID as an existing account.';
+        } else if (err.errno === 1452) {
+            error_message = 'Customer ID ' + request.body.idowner.toString() + ' does not exist.';
         }
-        else{
-            response.json(result);
+        return response.status(500).json({ status_code: response.statusCode, message: error_message }); 
         }
+        response.json(result);
     })
 });
 
@@ -43,8 +50,26 @@ router.put('/:idaccount_customer', validateFields(required_fields), function(req
         if(err){
             response.status(500).json({status_code: response.statusCode, message: err });
         }
+        if (result.affectedRows === 0) {
+            return response.status(404).json({ message: 'User not found.' });
+        }
         else{
-            response.json(result)
+            response.json(result);
+        }
+    })
+});
+
+router.patch('/:idaccount_customer', function(request, response){
+    account_customer.update(request.body, request.params.idaccount_customer, function(err, result){
+        if(err){
+            response.status(500).json({status_code: response.statusCode, message: err });
+        }
+        if (result.affectedRows === 0) {
+            return response.status(404).json({ message: 'User not found.' });
+        }
+
+        else{
+            response.json(result);
         }
     })
 });
@@ -55,7 +80,36 @@ router.delete('/:idaccount_customer', function(request, response){
             response.status(500).json({status_code: response.statusCode, message: err });
         }
         else{
-            response.json(result)
+            response.json(result);
+        }
+    })
+});
+
+router.get('/accountdata/:idaccount', function(request, response){
+    account_customer.accountData(request.params.idaccount, function(err, result){
+        if(err){
+            response.status(500).json({status_code: response.statusCode, message: err });
+        }
+        if (result[0].length === 0) {
+            return response.status(404).json({ status_code: response.statusCode, message: 'User not found.' });
+        }
+        else{
+            response.json(result[0]);
+        }
+    })
+});
+
+router.get('/customerdata/:idcustomer', function(request, response){
+    const idcustomer = parseInt(request.params.idcustomer);
+    account_customer.customerData(idcustomer, function(err, result){
+        if(err){
+            response.status(500).json({status_code: response.statusCode, message: err });
+        }
+        if (result[0].length === 0) {
+            return response.status(404).json({ status_code: response.statusCode, message: 'User not found.' });
+        }
+        else{
+            response.json(result[0]);
         }
     })
 });
