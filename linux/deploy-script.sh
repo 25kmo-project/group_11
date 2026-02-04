b3ad938ffa0997471ab4552acc4a9a71a19b857a
Tkarsikas <tommi.karsi@gmail.com>
2026-02-04 14:09:35 +0200

    fixes

--- linux/deploy-script.sh
+++ linux/deploy-script.sh
@@ -3,11 +3,6 @@ echo
 APP_DIR="/home/ubuntu/group_11/backend"
 APP_NAME="app.js"
 BRANCH="backend-linux"
-
-echo "update & upgrade"
-sudo apt update -y
-sudo apt upgrade -y
-
 echo "git pull..."
 cd $APP_DIR
 git fetch origin
@@ -16,7 +11,15 @@ git pull origin $BRANCH
 chmod +x /home/ubuntu/group_11/linux/deploy-backend.sh
 npm install
 
+echo "update & upgrade"
+sudo apt update -y
+sudo apt upgrade -y
+
 echo "pm2 reload"
 pm2 restart group11-backend
+
+echo "nginx restart..."
+sudo cp /home/ubuntu/group_11/linux/backend /home/ubuntu/etc/nginx/sites-available
+sudo systemctl reload nginx
 
 echo "deploy valmis"
