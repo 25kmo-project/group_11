58dbcb29781b84b65f09920191c8306e1d58ca70
Tommi <tommi.karsi@gmail.com>
2026-02-08 11:21:04 +0200

    Delete admin-app/build directory

--- admin-app/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_admin-app_FILE C:/GIT_Repositoryt/group_11/admin-app/build/admin-app.exe)
-set(__QT_DEPLOY_TARGET_admin-app_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_admin-app_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
