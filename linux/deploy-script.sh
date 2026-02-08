c6fe1df8f07b116211b10ba49ada87cee636419c
Tommi <tommi.karsi@gmail.com>
2026-02-08 11:31:12 +0200

    better comments for script

--- linux/deploy-script.sh
+++ linux/deploy-script.sh
@@ -4,7 +4,9 @@ APP_DIR="/home/ubuntu/group_11/backend"
 APP_NAME="app.js"
 BRANCH="main"
 
+echo "*********************************"
 echo "git pull..."
+echo "*********************************"
 cd $APP_DIR
 git fetch origin
 git checkout $BRANCH
@@ -12,15 +14,23 @@ git pull origin $BRANCH
 chmod +x /home/ubuntu/group_11/linux/deploy-backend.sh
 npm install
 
+echo "*********************************"
 echo "update & upgrade"
+echo "*********************************"
 sudo apt update -y
 sudo apt upgrade -y
 
+echo "*********************************"
 echo "pm2 reload"
+echo "*********************************"
 pm2 restart group11-backend
 
+echo "*********************************"
 echo "nginx restart..."
+echo "*********************************"
 sudo cp /home/ubuntu/group_11/linux/backend /etc/nginx/sites-available
 sudo systemctl reload nginx
 
-echo "deploy ready"
+echo "*********************************
+deploy ready
+*********************************"
