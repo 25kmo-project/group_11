1688a1a0dd1a00a6607430ae2cd97d86484000e4
Jesul1 <omajesse.vaarala@gmail.com>
2026-02-14 19:13:02 +0200

    wip:transaction_view

--- bank-automat/src/transaction_manager.cpp
+++ bank-automat/src/transaction_manager.cpp
@@ -7,6 +7,7 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonArray>
+#include <QUrlQuery>
 
 TransactionsManager::TransactionsManager(QObject* parent): QObject(parent) {
     manager = new QNetworkAccessManager(this);
@@ -48,13 +49,17 @@ void TransactionsManager::onTransactionsReply() {
     emit transactionsUpdated();
 }
 
-void TransactionsManager::fetchTransactions(const QString &accountId) {
-    const QString url =
-        environment::base_url() + "api/transaction/account/" + accountId;
+void TransactionsManager::fetchTransactions(const QString &accountId, int page, int limit) {
+    QUrl url(environment::base_url() + "api/transaction/account/" + accountId);
 
-    QNetworkRequest request{ QUrl(url) };
+    QUrlQuery query;
+    query.addQueryItem("page", QString::number(page));
+    query.addQueryItem("limit", QString::number(limit));
+    url.setQuery(query);
 
-    const QByteArray token =
+    QNetworkRequest request{ url };
+
+    QByteArray token =
         "Bearer " + AuthManager::instance()->getToken().toUtf8();
 
     request.setRawHeader("Authorization", token);
